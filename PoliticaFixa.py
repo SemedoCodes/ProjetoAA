711f4393773a1c54f9d73de1e95999daa034bc09
LuanaLopes27 <luana27092004@gmail.com>
2025-12-04 14:56:58 +0000

    PoliticaFixa.py

--- PoliticaFixa.py
+++ PoliticaFixa.py
@@ -1,4 +1,5 @@
 from Politica import Politica
+from Observacao import Observacao
 from Acao import Acao, TipoAcao
 from typing import Dict, Any
 
@@ -24,5 +25,26 @@ class PoliticaFixa(Politica):
         """
         Consulta as regras com base no que o agente viu.
         """
+        if obs.tem_vetor():
+            dx_total, dy_total = obs.vetor_alvo
 
+            # se ambos forem 0, estamos em cima do farol
+            if dx_total == 0 and dy_total == 0:
+                return Acao(TipoAcao.FAZER_NADA)
+
+            # se a distância horizontal for maior ou igual à vertical -> move em X
+            if abs(dx_total) >= abs(dy_total):
+                if dx_total > 0:
+                    return Acao.mover(1, 0)  # Mover Este
+                else:
+                    return Acao.mover(-1, 0)  # Mover Oeste
+
+            # mover em Y
+            else:
+                if dy_total > 0:
+                    return Acao.mover(0, 1)  # Mover Sul
+                else:
+                    return Acao.mover(0, -1)  # Mover Norte
+
+            return Acao(TipoAcao.FAZER_NADA)
         return (0, 0)
