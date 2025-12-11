cb587cb4167eee87ab5043eb17d7767162142234
SemedoCodes <stivejoe2710@gmail.com>
2025-12-11 22:21:05 +0000

    Guardar Politica Carregar Politica

--- Politica.py
+++ Politica.py
@@ -10,10 +10,17 @@ class Politica(ABC):
         """
         pass
 
-    @abstractmethod
+   
     def carregar_politica(self, ficheiro: str):
-        """
-        Carrega a política de um ficheiro (regras ou Q-Table).
-        """
-        pass
+        # carrega a Q-Table de um ficheiro txt
 
+        self.q_table = {}
+        with open(ficheiro, "r") as f:
+            for linha in f:
+                linha = linha.strip()
+                if not linha:
+                    continue
+                x_str, y_str, acao, q_str = linha.split(",")
+                estado = (int(x_str), int(y_str))
+                q = float(q_str)
+                self.q_table[(estado, acao)] = q   
--- PoliticaAprendizagem.py
+++ PoliticaAprendizagem.py
@@ -106,16 +106,4 @@ class PoliticaAprendizagem(Politica):
                f.write(f"{x},{y},{acao},{q}\n")
 
 
-    def carregar_politica(self, ficheiro: str):
-        # carrega a Q-Table de um ficheiro txt
-
-        self.q_table = {}
-        with open(ficheiro, "r") as f:
-            for linha in f:
-                linha = linha.strip()
-                if not linha:
-                    continue
-                x_str, y_str, acao, q_str = linha.split(",")
-                estado = (int(x_str), int(y_str))
-                q = float(q_str)
-                self.q_table[(estado, acao)] = q
+    
